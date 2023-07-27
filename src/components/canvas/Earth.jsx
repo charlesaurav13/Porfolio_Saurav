016f8350fefda0d760e3525295808088b05e01ae
SAURAV <81438796+charlesaurav13@users.noreply.github.com>
2023-07-27 12:45:58 +0530

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const moon = useGLTF("./moon/scene.gltf");
 
-  return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  return (<>
+    <primitive object={moon.scene} scale={2} position={[0,-2,0]} rotation-y={0} />
+    </>
   );
 };
 
@@ -16,7 +17,6 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
@@ -34,6 +34,16 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
+        <hemisphereLight intensity={1.5} groundColor='white' /> 
+        <spotLight
+        position={[-20,50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1.5} />
 
         <Preload all />
       </Suspense>
